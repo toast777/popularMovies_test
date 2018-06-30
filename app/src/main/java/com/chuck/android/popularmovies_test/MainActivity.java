b672b49ec0fcb169858b2d652f8de5beb5bdeae7
Chuck Robinson <gdgkctest@users.noreply.github.com>
2018-06-30 13:03:39 -0500

    Got RV to work

--- app/src/main/java/com/chuck/android/popularmovies_test/MainActivity.java
+++ app/src/main/java/com/chuck/android/popularmovies_test/MainActivity.java
@@ -2,12 +2,35 @@ package com.chuck.android.popularmovies_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.widget.LinearLayout;
+
+import com.chuck.android.popularmovies_test.adapter.MovieTitleAdapter;
+import com.chuck.android.popularmovies_test.models.MovieTitle;
+import com.chuck.android.popularmovies_test.utils.SampleData;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieTitleAdapter adapter;
+    LinearLayoutManager movieGridLayoutManger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.movieDbList);
+        movieGridLayoutManger = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(movieGridLayoutManger);
+        adapter = new MovieTitleAdapter(R.layout.movie_list_item, getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        List<MovieTitle> movies = SampleData.getMovies();
+        adapter.setMovies(movies);
+
     }
 }
